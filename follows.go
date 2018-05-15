fe2ba96d6e78dcd03045bbe988b4b22e57e894f9
Keith Alpichi <keithalpichi@gmail.com>
2018-05-14 22:43:45 -0700

    add blank follows functions

---
+++ follows.go
@@ -0,0 +1,13 @@
+package twitcher
+
+// UsersFollowing returns an array of users and an error for all users that
+// are follow the user `id`
+func (c Client) UsersFollowing(id string) (users []User, err error) {
+	return
+}
+
+// FollowedByUser returns an array of users and an error for all users that
+// are followed by the user `id`
+func (c Client) FollowedByUser(id string) (users []User, err error) {
+	return
+}
