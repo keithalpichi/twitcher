e2189f2238c09d613aa9d637e5f3172300d63e7e
Keith Alpichi <keithalpichi@gmail.com>
2018-05-14 22:21:22 -0700

    add/edit blank video functions

--- video.go
+++ video.go
@@ -1,17 +1,10 @@
-package twitch
+package twitcher
 
 import (
-	"net/url"
 	"time"
 )
 
-// Videor represents the interface for specific properties of Twitch Videos
-type Videor interface {
-	GetVideo(Twitcher, url.Values) (Video, error)
-	GetVideos(Twitcher, url.Values) ([]Video, error)
-}
-
-// Video contains meta information about a single Twitch video
+// Video contains meta information about a single Twitch video. More information at https://dev.twitch.tv/docs/api/reference#get-videos
 type Video struct {
 	ID           string    `json:"id"`
 	UserID       string    `json:"user_id"`
@@ -25,20 +18,30 @@ type Video struct {
 	Duration     string    `json:"duration"` // in format => "6h4m26s"
 }
 
-// VideosResponse represents a response from Twitch containing an array of Videos
-type VideosResponse struct {
+// videosResponse represents a response from Twitch containing an array of Videos
+type videosResponse struct {
 	Data       []Video `json:"data"`
 	Pagination struct {
 		Cursor string `json:"cursor"`
 	} `json:"pagination"`
 }
 
-// GetVideo gets a single Twitch video
-func (c Client) GetVideo(twitchClient Twitcher, v url.Values) (vid Video, err error) {
+// GetVideoByID gets a single Twitch video
+func (c Client) GetVideoByID(id string) (vid Video, err error) {
 	return
 }
 
-// GetVideos gets an array of Twitch videos
-func (c Client) GetVideos(twitchClient Twitcher, v url.Values) (vid []Video, err error) {
+// GetVideosByIDs gets an array of Twitch videos by their IDs
+func (c Client) GetVideosByIDs(ids []string) (vid []Video, err error) {
+	return
+}
+
+// GetVideosByUser gets an array of Twitch videos by user ID
+func (c Client) GetVideosByUser(id string) (vid []Video, err error) {
+	return
+}
+
+// GetVideosByGame gets an array of Twitch videos by game ID
+func (c Client) GetVideosByGame(id string) (vid []Video, err error) {
 	return
 }
